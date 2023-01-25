6880dffba54faa687c5555b32e36b96f4de989ee
Huskies xD <john.thor.peterson@gmail.com>
2023-01-25 20:34:27 +0000

    Added, Formatting and removed
    Added a new file for the Object Items in the server.lua file, this new file called Config.lua now contains those items.
    
    Added a reference to the config file in the fxmanifest.lua
    
    Formatting changed in the fxmanifest.lua
    
    Removed old items object from server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,19 +1,7 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 QBCore.Functions.CreateUseableItem("pdweaponcase", function(source, item)
-    local items = {
-        {name = "weapon_heavypistol", amount = 1},
-        {name = "weapon_stungun", amount = 1},
-        {name = "weapon_nightstick", amount = 1},
-        {name = "weapon_handcuffs", amount = 1},
-        {name = "radio", amount = 1},
-        {name = "weapon_flashlight", amount = 1},
-        {name = "heavyarmor", amount = 5},
-        {name = "pistol_ammo", amount = 5},
-        {name = "empty_evidence_bag", amount = 10}, -- copy and paste this line to add in more items.
-    }
-
-    for _, item in pairs(items) do
+    for _, item in pairs(Config.items) do
         QBCore.Functions.GetPlayer(source).Functions.AddItem(item.name, item.amount)
     end
     QBCore.Functions.GetPlayer(source).Functions.RemoveItem("pdweaponcase", 1)
