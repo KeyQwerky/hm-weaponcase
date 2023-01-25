56127363c70352f6bc12a29e7e677f73fbc7197b
KeyQwerky <hcaulcott01@gmail.com>
2023-01-25 12:14:41 +0000

    Final Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'adamant'
+game 'gta5'
+description 'Police Weapons Case Script made by hmods_tv'
+author 'hmods_tv#3918'
+version '1.0.0'
+
+client_scripts {'client/client.lua'}
+server_scripts {'server/server.lua'}
---
+++ server/server.lua
@@ -0,0 +1,31 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+QBCore.Functions.CreateUseableItem("pdweaponcase", function(source, item)
+    local src = source
+    local Ply = QBCore.Functions.GetPlayer(source)
+    local items = {
+        {name = "weapon_heavypistol", amount = 1},
+        {name = "weapon_stungun", amount = 1},
+        {name = "weapon_nightstick", amount = 1},
+        {name = "weapon_handcuffs", amount = 1},
+        {name = "radio", amount = 1},
+        {name = "weapon_flashlight", amount = 1},
+        {name = "heavyarmor", amount = 5},
+        {name = "pistol_ammo", amount = 5},
+        {name = "empty_evidence_bag", amount = 10}, -- copy and paste this line to add in more items.
+    }
+
+    for _, item in pairs(items) do
+        Ply.Functions.AddItem(item.name, item.amount, item.slot)
+    end
+    Ply.Functions.RemoveItem("pdweaponcase",1)
+    TriggerClientEvent('WeaponCase:Client:UseGiftBox', src, item)
+    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["pdweaponcase"], "remove")
+end)
+
+
+RegisterNetEvent('WeaponCase:server:recieveItem', function(item, itemAmount)
+    local src = source
+    local ply = QBCore.Functions.GetPlayer(src)
+    ply.Functions.AddItem(item, itemAmount)
+end)
